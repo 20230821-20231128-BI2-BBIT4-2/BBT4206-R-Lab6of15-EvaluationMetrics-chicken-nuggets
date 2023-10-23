47da669f04ec37639fab940305342750f14cb60f
Habiba2704 <84373596+Habiba2704@users.noreply.github.com>
2023-10-23 22:02:52 +0300

    Update Lab6-Submission-EvaluationMetrics.R
    Accuracy and Cohen's Kappa
    RMSE, R Squared and MAE

---
+++ Lab6-Submission-EvaluationMetrics.R
@@ -0,0 +1,99 @@
+## Loading required packages
+if (require("ggplot2")) {
+  require("ggplot2")
+} else {
+  install.packages("ggplot2", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+
+if (require("caret")) {
+  require("caret")
+} else {
+  install.packages("caret", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+
+if (require("mlbench")) {
+  require("mlbench")
+} else {
+  install.packages("mlbench", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+
+if (require("pROC")) {
+  require("pROC")
+} else {
+  install.packages("pROC", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+
+if (require("dplyr")) {
+  require("dplyr")
+} else {
+  install.packages("dplyr", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+# Accuracy and Cohen's Kappa
+# Loading the dataset
+library(readr)
+Customer_Churn <- read_csv("data/Customer Churn.csv", 
+    col_types = cols(Complains = col_factor(levels = c("1", 
+        "0")), `Tariff Plan` = col_factor(levels = c("1", 
+        "2")), Status = col_factor(levels = c("1", 
+        "2")), Churn = col_factor(levels = c("1", 
+        "0"))))
+View(Customer_Churn)  
+# Determining baseline accuracy
+Customer_Churn_freq <- Customer_Churn$Churn
+cbind(frequency =
+        table(Customer_Churn_freq),
+      percentage = prop.table(table(Customer_Churn_freq)) * 100)
+# Splitting the dataset
+train_index <- createDataPartition(Customer_Churn$Churn,
+                                   p = 0.80,
+                                   list = FALSE)
+Customer_Churn_train <- Customer_Churn[train_index, ]
+Customer_Churn_test <- Customer_Churn[-train_index, ]
+# Training the model; Applying 10 fold cross validation
+train_control <- trainControl(method = "cv", number = 10)
+Churn_model_glm <-
+  train(Churn ~ ., data = Customer_Churn_train, method = "glm",
+        metric = "Accuracy", trControl = train_control)
+# Displaying model's performance
+print(Churn_model_glm)
+
+# RMSE, R Squared and MAE
+# Loading dataset
+data("BostonHousing")
+#Splitting dataset
+set.seed(120)
+train.index <- sample(1:nrow(BostonHousing), 0.8 * nrow(BostonHousing))
+Boston_Train <- BostonHousing[train_index, ]
+Boston_test <- BostonHousing[-train_index, ]
+#Training the model
+train_control <- trainControl(method = "cv", number = 10)
+BostonHousing_model_lm <-
+  train(medv ~ ., data = Boston_Train,
+        na.action = na.omit, method = "lm", metric = "RMSE",
+        trControl = train_control)
+#Display model's performance
+print(BostonHousing_model_lm)
+
+#Logarithmic Loss
+
+
+
+
+
+
+
+
+
+
+
+
